188ce176969f409cc5bfe54c2a289a45bd9a332c
Abdulhakim <abdulhakimalfredi@gmail.com>
2026-01-14 15:43:08 +0300

    -

--- src/components/TheRoadMap.js
+++ src/components/TheRoadMap.js
@@ -2,16 +2,16 @@ import React from "react";
 import CategoryButton from "./CategoryButton.js";
 
 export default function SideMenu() {
-  const generalList = generalData.map((item) => (
-    <CategoryButton key={item.id} title={item.title} />
-  ));
+  // const generalList = generalData.map((item) => (
+  //   <CategoryButton key={item.id} title={item.title} />
+  // ));
 
-  const frontendList = frontendData.map((item) => (
-    <CategoryButton key={item.id} title={item.title} />
-  ));
-  const backendList = backendData.map((item) => (
-    <CategoryButton key={item.id} title={item.title} />
-  ));
+  // const frontendList = frontendData.map((item) => (
+  //   <CategoryButton key={item.id} title={item.title} />
+  // ));
+  // const backendList = backendData.map((item) => (
+  //   <CategoryButton key={item.id} title={item.title} />
+  // ));
 
   return (
     <div
@@ -23,30 +23,27 @@ export default function SideMenu() {
       }}
     >
       <h2>The Roadmap</h2>
-      <h3>General</h3>
-      {generalList}
-      <h3>FrontEnd</h3>
-      {frontendList}
-      <h3>BackEnd</h3>
-      {backendList}
+      <CategoryButton title="General"/>
+      <CategoryButton title="FrontEnd"/>
+      <CategoryButton title="BackEnd"/>
     </div>
   );
 }
 
-const generalData = [
-  { id: 1, title: "Scrum" },
-  { id: 2, title: "Git" },
-];
+// const generalData = [
+//   { id: 1, title: "Scrum" },
+//   { id: 2, title: "Git" },
+// ];
 
-// 2. مصفوفة الفرونت إند
-const frontendData = [
-  { id: 3, title: "JavaScript" },
-  { id: 4, title: "React" },
-];
+// // 2. مصفوفة الفرونت إند
+// const frontendData = [
+//   { id: 3, title: "JavaScript" },
+//   { id: 4, title: "React" },
+// ];
 
-// 3. مصفوفة الباك إند
-const backendData = [
-  { id: 5, title: "SQL" },
-  { id: 6, title: "PHP" },
-  { id: 7, title: "Laravel" },
-];
+// // 3. مصفوفة الباك إند
+// const backendData = [
+//   { id: 5, title: "SQL" },
+//   { id: 6, title: "PHP" },
+//   { id: 7, title: "Laravel" },
+// ];
