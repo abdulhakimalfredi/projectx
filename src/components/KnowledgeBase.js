6ad0fe4ccf49b2178af05b62d672b8b4973123f1
Abdulhakim <abdulhakimalfredi@gmail.com>
2026-01-18 12:51:02 +0300

    -

--- src/components/KnowledgeBase.js
+++ src/components/KnowledgeBase.js
@@ -19,25 +19,25 @@ export default function KnowledgeBase({ActivePath}) {
                 <a href="https://www.youtube.com/watch?v=cJH0xlFR3Qs" className="resource-link">
                   Scrum in 10 minutes
                 </a>
-                <CategoryButton title="Done ✓"/>
+                <input type="checkbox"/>
               </div>
               <div className="resource-row">
                 <a href="https://www.youtube.com/watch?v=xoNlhLAKly4" className="resource-link">
                   Introduction to Scrum [Arabic]
                 </a>
-                <CategoryButton title="Done ✓"/>
+                <input type="checkbox"/>
               </div>
               <div className="resource-row">
                 <a href="https://www.scrum.org/resources/scrum-guide" className="resource-link">
                   The Scrum Guide
                 </a>
-                <CategoryButton title="Done ✓"/>
+                <input type="checkbox"/>
               </div>
               <div className="resource-row">
                 <a href="https://www.scrum.org/open-assessments/scrum-open" className="resource-link">
                   Scrum Open assessment
                 </a>
-                <CategoryButton title="Done ✓"/>
+                <input type="checkbox"/>
               </div>
             </div>
           </div>
@@ -49,13 +49,13 @@ export default function KnowledgeBase({ActivePath}) {
                 <a href="https://www.youtube.com/watch?v=Z6C35WqawGs" className="resource-link">
                   GitHub Playlist
                 </a>
-                <CategoryButton title="Done ✓" />
+                <input type="checkbox"/>
               </div>
               <div className="resource-row">
                 <a href="https://chris.beams.io/git-commit" className="resource-link">
                   How to Write a Git Commit Message
                 </a>
-                <CategoryButton title="Done ✓" />
+                <input type="checkbox"/>
               </div>
             </div>
           </div>
@@ -71,13 +71,13 @@ export default function KnowledgeBase({ActivePath}) {
               <a href="https://www.youtube.com/watch?v=2EAV2cB3FWY" className="resource-link">
                 JavaScript Course
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
             <div className="resource-row">
               <a href="https://www.youtube.com/playlist?list=PLYyqC4bNbCIdSZ-JayMLl4WO2Cr995vyS" className="resource-link">
                 React Course
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
           </div>
         </div>
@@ -92,25 +92,25 @@ export default function KnowledgeBase({ActivePath}) {
               <a href="https://satr.tuwaiq.edu.sa/course/FtkmhtJpQW/view" className="resource-link">
                 SQL 101
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
             <div className="resource-row">
               <a href="https://satr.tuwaiq.edu.sa/course/APjgdQqVWR/view" className="resource-link">
                 SQL 102
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
             <div className="resource-row">
               <a href="https://satr.tuwaiq.edu.sa/course/bOXiOFzkMv/view" className="resource-link">
                 SQL 103
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
             <div className="resource-row">
               <a href="https://www.datacamp.com/tutorial/normalization-in-dbms" className="resource-link">
                 Normalization in DBMS (article)
               </a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
           </div>
 
@@ -118,11 +118,11 @@ export default function KnowledgeBase({ActivePath}) {
             <h3>PHP & Laravel</h3>
             <div className="resource-row">
               <a href="#" className="resource-link">PHP for Beginners</a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
             <div className="resource-row">
               <a href="#" className="resource-link">Laravel Framework</a>
-              <CategoryButton title="Done ✓" />
+              <input type="checkbox"/>
             </div>
           </div>
         </div>
